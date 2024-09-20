d0a2d07abb4c8775f769f605b376a123408f0689
nonorze-VOXX <appieosugood@gmail.com>
2024-09-20 10:53:51 -0400

    waybar hyprland config update

---
+++ hypr/link.sh
@@ -0,0 +1,7 @@
+rm ~/.config/hypr/*
+ln hypridle.conf ~/.config/hypr/
+ln hyprpaper.conf ~/.config/hypr/
+ln hyprlock.conf ~/.config/hypr/
+ln hyprland.conf ~/.config/hypr/
+ln suspend.sh ~/.config/hypr/
+ln volume-up.sh ~/.config/hypr/
